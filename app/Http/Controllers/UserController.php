fdda7f736f5001aca9b9092f430c63c682555ad4
Nuel Agyei <nuelagyei992@gmail.com>
2023-09-18 11:32:54 +0000

    userController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request) {
+        $fields = $request->validate([
+            'name'=> 'required',
+            'email'=>'required',
+            'password'=>'required'
+        ]);
+        return 'Hello This be the controller';
+    }
 }
